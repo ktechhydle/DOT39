4a7216e0dabeb1f7707784de26403e0cef094b0f
kelle <kellerhydle@gmail.com>
2024-12-10 16:25:40 -0700

    Updates
    -added outline to terrain item

--- src/framework/items/terrain_item.py
+++ src/framework/items/terrain_item.py
@@ -1,29 +1,40 @@
 from src._imports import *
 from src.framework.items.base_item import *
 from src.framework.scene.functions import hexToRGB
+from scipy.spatial import ConvexHull
 
 
 class TerrainItem(BaseItem):
     def __init__(self, scene, program, points: list[tuple[float, float, float]]):
         super().__init__(scene)
         self._color = hexToRGB('#00ff00')
+        self._outline_color = hexToRGB('#00ff00')
         self._points = points
 
         self.program = program
         self.ctx = scene.ctx
         self.vbo = self.createVbo()
+        self.outline_vbo = self.createOutlineVbo()  # Add outline VBO
 
     def color(self):
         return self._color
 
+    def outlineColor(self):
+        return self._outline_color
+
     def setColor(self, color: str):
         self._color = color
+
+    def setOutlineColor(self, color: str):
+        self._outline_color = color
 
     def points(self):
         return self._points
 
     def setPoints(self, points: list[tuple[float, float, float]]):
         self._points = points
+        self.vbo = self.createVbo()
+        self.outline_vbo = self.createOutlineVbo()  # Update outline VBO
 
     def createVbo(self):
         array = []
@@ -32,18 +43,44 @@ class TerrainItem(BaseItem):
             array.extend([point[0] / 100, point[1] / 100, point[2] / 100])
 
         vertices = np.array(array, dtype='f4')
+        vbo = self.ctx.buffer(vertices)
+        return vbo
 
+    def createOutlineVbo(self):
+        if len(self.points()) < 3:
+            return None  # Not enough points for an outline
+
+        points_2d = np.array([(p[0], p[1]) for p in self.points()])
+        hull = ConvexHull(points_2d)
+        edges = []
+
+        # Add edges of the convex hull
+        for simplex in hull.simplices:
+            edges.extend([points_2d[simplex[0]], points_2d[simplex[1]]])
+
+        # Flatten and scale
+        edges_flat = []
+        for edge in edges:
+            edges_flat.extend([edge[0] / 100, edge[1] / 100, 0.0])  # Assume z=0 for outline
+
+        vertices = np.array(edges_flat, dtype='f4')
         vbo = self.ctx.buffer(vertices)
         return vbo
 
     def render(self):
         super().render()
 
-        # Use the shader program and draw
+        # Render points
         self.program['model'].write(np.eye(4, dtype='f4').tobytes())
         self.program['color'].value = self.color()
         self.program['alphaValue'].value = 1.0
 
         vao = self.ctx.simple_vertex_array(self.program, self.vbo, 'in_vert')
         vao.render(POINTS)
+
+        # Render outline
+        if self.outline_vbo:
+            self.program['color'].value = self.outlineColor()  # Outline color
+            outline_vao = self.ctx.simple_vertex_array(self.program, self.outline_vbo, 'in_vert')
+            outline_vao.render(LINES)
 
