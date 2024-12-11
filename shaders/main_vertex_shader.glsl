cd7236689e7af24e21946b6edf0d1eac14e7d119
kelle <kellerhydle@gmail.com>
2024-12-11 10:51:58 -0700

    Updates
    -added shader attributes

--- shaders/main_vertex_shader.glsl
+++ shaders/main_vertex_shader.glsl
@@ -2,13 +2,11 @@
 
 in vec3 in_vert;
 uniform mat4 model;
-uniform vec3 position;
+uniform vec3 position; // Item rendered position
 
 uniform float aspectRatio;
 uniform vec3 cameraPosition;
 uniform float cameraZoom;
-uniform float cameraYaw;
-uniform float cameraPitch;
 
 void main() {
     vec4 transformed = model * vec4(in_vert, 1.0);
