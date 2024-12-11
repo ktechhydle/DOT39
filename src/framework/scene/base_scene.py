b0d00d99e50c805fd0841a4b53312074fce2b8ca
kelle <kellerhydle@gmail.com>
2024-12-11 11:25:40 -0700

    Updates
    -added more gl updates and improved code

--- src/framework/scene/base_scene.py
+++ src/framework/scene/base_scene.py
@@ -14,27 +14,28 @@ class BaseScene(QOpenGLWidget):
         self.setMouseTracking(True)
         self.setCursor(Qt.CursorShape.CrossCursor)
 
-        self.unit_manager = UnitManager()
-
         self.undo_stack = QUndoStack(self)
         self.undo_stack.setUndoLimit(200)
 
         self.ctx = None
         self.program = None
         self.wireframe = True
+        self.bg_color = hexToRGB('#000000')
 
         self._items = []
         self._selected_items = []
 
     def initializeGL(self):
         self.ctx = moderngl.create_context()
-        self.ctx.clear(0, 0, 0)
+        self.ctx.clear(self.bg_color[0], self.bg_color[1], self.bg_color[2])
         self.ctx.wireframe = self.wireframe
 
         self.program = self.ctx.program(
             vertex_shader=vertex_shad,
             fragment_shader=fragment_shad
         )
+
+        self.ctx.finish()
 
     def resizeGL(self, w, h):
         width = max(2, w)
@@ -43,7 +44,10 @@ class BaseScene(QOpenGLWidget):
 
         self.program['aspectRatio'].value = self.width() / max(1.0, self.height())
 
+        self.update()
+
     def paintGL(self):
+        self.ctx.clear(self.bg_color[0], self.bg_color[1], self.bg_color[2])
         self.ctx.enable(DEPTH_TEST)
 
         item = TerrainItem(self, self.program, [(0.0, 0.0, 0.0),
@@ -59,6 +63,10 @@ class BaseScene(QOpenGLWidget):
 
         for item in self.items():
             item.render()
+
+        # Console Info
+        print('Current Color: ', self.program['color'].value)
+        print('Current Alpha Value: ', self.program['alphaValue'].value)
 
     def mousePressEvent(self, event):
         if event.button() == Qt.MouseButton.MiddleButton:
