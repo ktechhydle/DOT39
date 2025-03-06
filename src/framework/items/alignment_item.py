e93da4d2d37a1ebed4eaa3cbd72591755f2c8e60
kelle <kellerhydle@gmail.com>
2025-03-06 11:06:56 -0700

    Item Updates
    -added vertical path to alignment item

--- src/framework/items/alignment_item.py
+++ src/framework/items/alignment_item.py
@@ -13,6 +13,7 @@ class AlignmentItem(BaseItem):
         self.program = program
         self.ctx = scene.ctx
         self._horizontal_path = QPainterPath()
+        self._vertical_path = QPainterPath()
         self._draw_calls = []
         self.vbo = self.createVbo()
 
@@ -26,7 +27,13 @@ class AlignmentItem(BaseItem):
                 for point in polygon:
                     vertices.append(point.x())
                     vertices.append(point.y())
-                    vertices.append(0)
+
+                    if not self._vertical_path.isEmpty():
+                        for pg in self._vertical_path.toSubpathPolygons():
+                            for p in pg:
+                                vertices.append(p.y())
+                    else:
+                        vertices.append(0)
 
             return self.ctx.buffer(np.array(vertices, dtype='f4'))
 
@@ -64,8 +71,6 @@ class AlignmentItem(BaseItem):
             y = center_y + radius * np.sin(theta)
             self._horizontal_path.lineTo(x, y)
 
-        self._draw_calls.append({'Circular Curve': (to_x, to_y)})
-
         self.update()
 
     def drawClothoid(self, to_x, to_y, num_points=100, a=1.0):
@@ -89,7 +94,6 @@ class AlignmentItem(BaseItem):
 
             self._horizontal_path.lineTo(x, y)
 
-        self._draw_calls.append({'Clothoid Curve': (to_x, to_y)})
         self.update()
 
     def drawLine(self, to_x, to_y):
@@ -116,13 +120,26 @@ class AlignmentItem(BaseItem):
 
         self.update()
 
+    def clearVerticalPath(self):
+        self._vertical_path.clear()
+
+        self.update()
+
     def setHorizontalPath(self, path: QPainterPath):
         self._horizontal_path = path
 
         self.update()
 
+    def setVerticalPath(self, path: QPainterPath):
+        self._vertical_path = path
+
+        self.update()
+
     def horizontalPath(self):
         return self._horizontal_path
+
+    def verticalPath(self):
+        return self._vertical_path
 
     def render(self, color=None):
         super().render()
