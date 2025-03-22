177381aeebaa8525440c7a582f78561948eef734
kelle <kellerhydle@gmail.com>
2025-03-22 17:23:58 -0600

    UI Updates
    -context menu action for editing values

--- src/framework/managers/context_menu_manager.py
+++ src/framework/managers/context_menu_manager.py
@@ -2,6 +2,7 @@ from src._imports import *
 from src.gui.widgets import ContextMenu
 from src.framework.items.point_group import PointGroupItem
 from src.framework.items.alignment_item import AlignmentItem
+from src.framework.items.editable_item import EditableItem
 
 
 class ContextMenuManager(object):
@@ -35,6 +36,12 @@ class ContextMenuManager(object):
                 edit_alignment_action.triggered.connect(self.parent.alignmentManager.editAlignment)
 
                 menu.addAction(edit_alignment_action)
+
+            elif isinstance(selected_item, EditableItem):
+                edit_value_action = QAction('Edit Value', menu)
+                edit_value_action.triggered.connect(selected_item.startEditing)
+
+                menu.addAction(edit_value_action)
 
         select_all_action = QAction('Select All', menu)
         select_all_action.triggered.connect(self.scene.selectionTool().selectAll)
